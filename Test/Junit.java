e2292650785ead94a7edab66dd17d008fbb5fa74
hora459 <modysamir459@gmail.com>
2023-05-21 17:06:23 +0300

    add HashTable interface

--- Test/Junit.java
+++ Test/Junit.java
@@ -1,160 +1,174 @@
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.awt.*;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
-
-public class Junit<T extends Comparable<T>> {
-    Dictionary d1 = new Dictionary(1); // AVL
-    Dictionary d2 = new Dictionary(2); // Red black
-    int test1_getSize = 84402;
-    int test2_getSize = 10000;
-    int test10_getSize = 466472;
-
-    // AVL TESTS
+public class Junit {
+    private UniversalHashing<Integer> hashing=new UniversalHashing<>(5);
 
     @Test
-    public void single_insert_avl_test() {
-        d1.insert("apple");
-        assertEquals(1, d1.getSize());
-        assertEquals(1, d1.getHeight());
-        assertTrue(d1.search("apple"));
+    public void testInsert() {
+        hashing.insert(5);
+        Assert.assertTrue(hashing.search(5));
     }
 
     @Test
-    public void single_delete_avl_test() {
-        d1.insert("apple");
-        assertEquals(1, d1.getSize());
-        assertEquals(1, d1.getHeight());
-        d1.delete("apple");
-        assertEquals(0, d1.getSize());
-        assertEquals(0, d1.getHeight());
-        assertFalse(d1.search("apple"));
+    public void testDelete() {
+        hashing.insert(5);
+        hashing.delete(5);
+        Assert.assertFalse(hashing.search(5));
     }
 
     @Test
-    public void batch_insert_avl_test() {
-        d1.batch_insert("test1.txt");
-        assertEquals(test1_getSize, d1.getSize());
-        assertEquals(19, d1.getHeight());
-        d1.batch_insert("test1.txt"); // insert again but still the same
-        assertEquals(test1_getSize, d1.getSize());
-        assertEquals(19, d1.getHeight());
+    public void testingBatchInsert() {
+        Integer[] keys = {1, 2, 3, 4, 5};
+        Integer[] keys2 = {6, 7, 8, 9, 10};
+        hashing.batchInsert(keys);
+        hashing.batchInsert(keys2);
+        for (Integer key : keys) {
+            Assert.assertTrue(hashing.search(key));
+        }
+        for (Integer key : keys2) {
+            Assert.assertTrue(hashing.search(key));
+        }
     }
 
     @Test
-    public void batch_insert_empty_avl_test() {
-        d1.batch_insert("empty.txt");
-        assertEquals(0, d1.getSize());
-        assertEquals(0, d1.getHeight());
+    public void testBatchDelete() {
+        Integer[] keys = {1, 2, 3, 4, 5};
+        Integer[] keys2 = {6, 7, 8, 9, 10};
+        hashing.batchInsert(keys);
+        hashing.batchDelete(keys);
+        for (Integer key : keys) {
+            Assert.assertFalse(hashing.search(key));
+        }
+        for (Integer key : keys2) {
+            Assert.assertFalse(hashing.search(key));
+        }
     }
 
     @Test
-    public void batch_delete_avl_test() {
-        d1.batch_insert("test1.txt");
-        d1.batch_delete("test1.txt");
-        assertEquals(0, d1.getSize());
-        d1.batch_insert("test2.txt");
-        d1.batch_delete("test2.txt");
-        assertEquals(0, d1.getSize());
+    public void testInsertAndContains() {
+        UniversalHashing<String> hashTable = new UniversalHashing<>(5);
+
+        // Insert some keys
+        hashTable.insert("apple");
+        hashTable.insert("banana");
+        hashTable.insert("cherry");
+
+        // Check if the keys are present
+        Assert.assertTrue(hashTable.search("apple"));
+        Assert.assertTrue(hashTable.search("banana"));
+        Assert.assertTrue(hashTable.search("cherry"));
+
+        // Check for non-existent key
+        Assert.assertFalse(hashTable.search("orange"));
     }
 
     @Test
-    public void insert_bigdata_avl_test() {
-        d1.batch_insert("test8.txt");
-        System.out.println(d1.getHeight());
-    }
+    public void testCollisionDetection() {
+        // Create an array with keys that are known to cause collisions
+        Integer[] keys = {1, 2, 3, 4, 5};
 
+        UniversalHashing<Integer> hashTable = new UniversalHashing<>(keys);
 
-    // RED BLACK TESTS
+        // Check if collision is detected
+        Assert.assertFalse(hashTable.isCollisionDetected());
 
-    @Test
-    public void single_insert_rb_test() {
-        d2.insert("apple");
-        assertEquals(1, d2.getSize());
-        assertEquals(1, d2.getHeight());
-        assertTrue(d2.search("apple"));
-    }
-
-    @Test
-    public void single_delete_rb_test() {
-        d2.insert("apple");
-        assertEquals(1, d2.getSize());
-        assertEquals(1, d2.getHeight());
-        d2.delete("apple");
-        assertEquals(0, d2.getSize());
-        assertEquals(0, d2.getHeight());
-        assertFalse(d2.search("apple"));
+        // Check the collision count
+        System.out.println("Collision count: " + hashTable.getCollisionCount());
+        Assert.assertEquals(true,hashTable.getCollisionCount()<=5);
     }
 
     @Test
-    public void batch_insert_rb_test() {
-        d2.batch_insert("test1.txt");
-        assertEquals(test1_getSize, d2.getSize());
-        d2.batch_insert("test1.txt"); // insert again but still the same
-        assertEquals(test1_getSize, d2.getSize());
+    public void testEmptyHashTable() {
+        UniversalHashing<String> hashTable = new UniversalHashing<>(10);
+
+        // Check for non-existent key in an empty hash table
+        Assert.assertFalse(hashTable.search("apple"));
     }
 
     @Test
-    public void batch_insert_empty_rb_test() {
-        d2.batch_insert("empty.txt");
-        assertEquals(0, d2.getSize());
-        assertEquals(0, d2.getHeight());
+    public void testLargeHashTable() {
+        // Create a large hash table with 1000 elements
+        UniversalHashing<Integer> hashTable = new UniversalHashing<>(1000);
+
+        // Insert elements from 0 to 999
+        for (int i = 0; i < 1000; i++) {
+            hashTable.insert(i);
+        }
+
+        // Check if all elements are present
+        for (int i = 0; i < 1000; i++) {
+            Assert.assertTrue(hashTable.search(i));
+        }
     }
 
     @Test
-    public void batch_delete_rb_test() {
-        d2.batch_insert("test1.txt");
-        d2.batch_delete("test1.txt");
-        assertEquals(0, d2.getSize());
-        d2.batch_insert("test2.txt");
-        d2.batch_delete("test2.txt");
-        assertEquals(0, d2.getSize());
+    public void testBatchInsert() {
+        int size = 10000; // Size of the array
+        Integer[] keys = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            keys[i] = i;
+        }
+
+        UniversalHashing<Integer> hashTable = new UniversalHashing<>(keys);
+
+        // Check if all keys are present in the hash table
+        for (int i = 0; i < size; i++) {
+            Assert.assertTrue(hashTable.search(i));
+        }
+
+        // Check if a collision occurred during insertion
+        Assert.assertFalse(hashTable.isCollisionDetected());
+        System.out.println("Collision count: " + hashTable.getCollisionCount());
     }
 
     @Test
-    public void insert_bigdata_rb_test() {
-        d2.batch_insert("test8.txt");
-        System.out.println(d2.getHeight());
+    public void testRebuildCount() {
+        Integer[] keys = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        UniversalHashing<Integer> hashing = new UniversalHashing<>(keys);
+
+        int rebuildCount = hashing.getRebuildCount();
+        System.out.println("Initial rebuild count: " + rebuildCount);
+
+        // Insert a key causing a collision
+        hashing.insert(11);
+
+        rebuildCount = hashing.getRebuildCount();
+        System.out.println("Rebuild count after 1st collision: " + rebuildCount);
+
+        // Insert more keys causing collisions
+        hashing.insert(12);
+        hashing.insert(13);
+
+        rebuildCount = hashing.getRebuildCount();
+        System.out.println("Rebuild count after 2nd and 3rd collision: " + rebuildCount);
     }
-
-    //Comparsion
-
     @Test
-    public void operations_time() {
-        long start1 = System.nanoTime();
-        d2.batch_insert("test8.txt");
-        long end1 = System.nanoTime();
+    public void testSpaceComplexity() {
+        int numElements = 1000; // Number of elements to insert
+        UniversalHashing<String> hashTable = new UniversalHashing<>(numElements);
 
-        long start2 = System.nanoTime();
-        d2.search("nonatomical");
-        d2.search("alreadyexist");
-        long end2 = System.nanoTime();
+        // Insert elements into the hash table
+        for (int i = 0; i < numElements; i++) {
+            String key = "key_" + i;
+            hashTable.insert(key);
+        }
 
-        long start3 = System.nanoTime();
-        d2.delete("parade");
-        long end3 = System.nanoTime();
+        long memoryUsage = getMemoryUsage(hashTable);
+        long expectedSpaceComplexity = numElements * numElements;
 
-        long start4 = System.nanoTime();
-        d1.batch_insert("test8.txt");
-        long end4 = System.nanoTime();
-
-        long start5 = System.nanoTime();
-        d1.search("nonatomical");
-        d1.search("alreadyexist");
-        long end5 = System.nanoTime();
-
-        long start6 = System.nanoTime();
-        d1.delete("parade");
-        long end6 = System.nanoTime();
-
-        System.out.println("\u001B[35mTime taken to insert in AVL tree = (" + (end4 - start4) + ") nanoseconds\u001B[0m");
-        System.out.println("\u001B[35mTime taken to search in AVL tree = (" + (end5 - start5) + ") nanoseconds\u001B[0m");
-        System.out.println("\u001B[35mTime taken to delete in AVL BLACK tree = (" + (end6 - start6) + ") nanoseconds\u001B[0m");
-
-        System.out.println("\u001B[34mTime taken to insert in RED BLACK tree = (" + (end1 - start1) + ") nanoseconds\u001B[0m");
-        System.out.println("\u001B[34mTime taken to search in RED BLACK tree = (" + (end2 - start2) + ") nanoseconds\u001B[0m");
-        System.out.println("\u001B[34mTime taken to delete in RED BLACK tree = (" + (end3 - start3) + ") nanoseconds\u001B[0m");
+        // Verify that the memory usage is approximately O(N^2)
+        assert memoryUsage >= expectedSpaceComplexity : "Space complexity violated!";
     }
 
+    private long getMemoryUsage(Object obj) {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc(); // Run garbage collector to release memory
+        long memoryUsed = runtime.totalMemory() - runtime.freeMemory();
+        return memoryUsed;
+    }
 }
+
+
--- src/UniversalHashing.java
+++ src/UniversalHashing.java
@@ -1,89 +1,177 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class UniversalHashing <T extends Comparable<T>> {
+public class UniversalHashing <T extends Comparable<T>> implements HashTable<T>{
     private int size;
     private List<T>[] table;
-    private int [][] hashFunction;
+    private int[][] hashFunction;
     private Random random;
+    private boolean collisionDetected;
+    private int collisionCount;
+    private int rebuildCount;
 
     public UniversalHashing(T[] keys) {
         size = keys.length;
         random = new Random();
         // Create the hash table
         table = new List[size * size];
-        for (int i = 0; i < size * size; i++) {
-            table[i] = new ArrayList<>();
-        }
+        collisionDetected = false;
+        collisionCount = 0;
+        rebuildCount = 0;
         buildHashTable(keys);
     }
 
+    public UniversalHashing(int size) {
+        this.size = size;
+        random = new Random();
+        // Create the hash table
+        table = new List[size * size];
+        generateHashFunction();
+    }
+
+    @Override
+    public void insert(T key) {
+        int index = calculateIndex(key);
+        if (table[index] == null) {
+            table[index] = new ArrayList<>();
+        }
+        table[index].add(key);
+
+        // Handle collision
+        if (table[index].size() > 1) {
+            List<T> elementsToReinsert = new ArrayList<>();
+            for (List<T> bin : table) {
+                if (bin != null) {
+                    elementsToReinsert.addAll(bin);
+                }
+            }
+            Arrays.fill(table, null);
+            // Reinsert the elements
+            T[] reinsertArray = toArrayWithElementType(elementsToReinsert);
+            buildHashTable(reinsertArray);
+        }
+    }
+    @SuppressWarnings("unchecked")
+    private T[] toArrayWithElementType(List<T> list) {
+        T[] array = (T[]) new Comparable[list.size()];
+        return list.toArray(array);
+    }
+
+
+    @Override
+    public void delete(T key) {
+        int index = calculateIndex(key);
+        if (table[index] != null) {
+            table[index].remove(key);
+        }
+    }
+
+    @Override
+    public void batchInsert(T[] keys) {
+        for (T key : keys) {
+            insert(key);
+        }
+    }
+
+    @Override
+    public void batchDelete(T[] keys) {
+        for (T key : keys) {
+            delete(key);
+        }
+    }
+
     private void buildHashTable(T[] keys) {
-        boolean collisions;
         do {
-            collisions = false;
+            collisionDetected = false;
+            generateHashFunction();
 
-            // Generate a random hash function
-            hashFunction = generateHashFunction();
-
-            // Hash the keys into the table
             for (T key : keys) {
-                int index = calculateIndex(hashFunction, key);
-                if (table[index].size() > 0) {
-                    collisions = true;
-                    System.out.println("Collision detected!");
+                int index = calculateIndex(key);
+                if (table[index] != null && !table[index].isEmpty()) {
+                    collisionDetected = true;
+                    collisionCount++;
                     break;
+                }
+                if (table[index] == null) {
+                    table[index] = new ArrayList<>();
                 }
                 table[index].add(key);
             }
 
-            // Clear the table if collisions occurred
-            if (collisions) {
-                for (List<T> bin : table) {
-                    bin.clear();
-                }
+            if (collisionDetected) {
+                rebuildCount++;
+                table = new List[size * size]; // Clear the table if collisions occurred
             }
-        } while (collisions);
+        } while (collisionDetected);
     }
 
-    private int[][] generateHashFunction() {
-        int[][] hashFunction = new int[log2(size*size)][];
-        for (int i = 0; i < log2(size*size); i++) {
-            hashFunction[i] = new int[32]; // Assuming 32-bit integers
+    private void generateHashFunction() {
+        hashFunction = new int[log2(size * size)][32];
+        for (int i = 0; i < log2(size * size); i++) {
             for (int j = 0; j < 32; j++) {
                 hashFunction[i][j] = random.nextInt(2);
             }
         }
-        return hashFunction;
     }
 
-    private int calculateIndex(int[][] hashFunction, T key) {
+    private int calculateIndex(T key) {
         int index = 0;
-        for (int i = 0; i < log2(size*size); i++) {
+        for (int i = 0; i < log2(size * size); i++) {
             int[] hash = hashFunction[i];
-            int h = 0;
-            for (int j = 0; j < 32; j++) {
-                if (hash[j] == 1) {
-                    h ^= (key.hashCode() >> j) & 1;
-                }
-            }
+            int h = dotProduct(hash, key);
             index = (index << 1) | h;
         }
-        return index%(size*size);
+        return index % (size * size);
     }
 
-    public boolean contains(T key) {
-        int index = calculateIndex(hashFunction, key);
-        return table[index].contains(key);
-    }
-    public  void print(){
-        for (int i = 0; i < size*size; i++) {
-            System.out.println(table[i]);
+    private int dotProduct(int[] hash, T key) {
+        int result = 0;
+        int[] keyBits = toBinaryArray(key.hashCode());
+        for (int i = 0; i < 32; i++) {
+            result += hash[i] * keyBits[i];
         }
+        return result % 2;
     }
+
+    private int[] toBinaryArray(int n) {
+        int[] binaryArray = new int[32];
+        for (int i = 31; i >= 0; i--) {
+            binaryArray[i] = n & 1;
+            n >>= 1;
+        }
+        return binaryArray;
+    }
+
     private int log2(int n) {
         return (int) (Math.log(n) / Math.log(2));
     }
 
+    @Override
+    public boolean search(T key) {
+        int index = calculateIndex(key);
+        if (table[index] != null) {
+            return table[index].contains(key);
+        }
+        return false;
+    }
+
+    public void print() {
+        for (int i = 0; i < size * size; i++) {
+            System.out.println(table[i]);
+        }
+    }
+
+    public boolean isCollisionDetected() {
+        return collisionDetected;
+    }
+
+    public int getCollisionCount() {
+        return collisionCount;
+    }
+
+    public int getRebuildCount() {
+        return rebuildCount;
+    }
 }
